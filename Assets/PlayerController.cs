60de07e1e59634860391b97545753c93eaccf4cf
humberd <humberd2@gmail.com>
2020-06-26 17:07:36 +0200

    Player is moving

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _controller;
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        _controller.Move(move * (Time.deltaTime * speed));
+    }
+
+}
