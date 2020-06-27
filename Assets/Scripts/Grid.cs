9897f3064b58c1d052eb43a69c76e41d457b20a8
humberd <humberd2@gmail.com>
2020-06-27 17:24:06 +0200

    Properly sets up stuff

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,4 +1,5 @@
-﻿using CodeMonkey.Utils;
+﻿using System;
+using CodeMonkey.Utils;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ public class Grid
     private int _height;
     private float _cellSize;
     private int[,] _gridArray;
+    private TextMesh[,] _textMeshes;
 
     public Grid(int width, int height, float cellSize)
     {
@@ -15,6 +17,7 @@ public class Grid
         _height = height;
         _cellSize = cellSize;
         _gridArray = new int[width, height];
+        _textMeshes = new TextMesh[width, height];
 
         foreach (var i in _gridArray)
         {
@@ -24,7 +27,7 @@ public class Grid
         {
             for (int y = 0; y < _gridArray.GetLength(1); y++)
             {
-                UtilsClass.CreateWorldText(_gridArray[x, y].ToString(), null,
+                _textMeshes[x, y] = UtilsClass.CreateWorldText(_gridArray[x, y].ToString(), null,
                     GetWorldPosition(x, y) + new Vector3(_cellSize / 2, _cellSize / 2), 30, Color.white,
                     TextAnchor.MiddleCenter);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
@@ -36,8 +39,25 @@ public class Grid
         Debug.DrawLine(GetWorldPosition(_width, 0), GetWorldPosition(_width, _height), Color.white, 100f);
     }
 
-    private Vector3 GetWorldPosition(int x, int y)
+    public Vector3 GetWorldPosition(int x, int y)
     {
         return new Vector3(x, y) * _cellSize;
+    }
+
+    private Vector2Int GetCoordinates(Vector3 vector3)
+    {
+        return new Vector2Int((int) Math.Floor(vector3.x / _cellSize), (int) Math.Floor(vector3.y / _cellSize));
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        _gridArray[x, y] = value;
+        _textMeshes[x, y].text = _gridArray[x, y].ToString();
+    }
+
+    public void SetValue(Vector3 vector3, int value)
+    {
+        var coords = GetCoordinates(vector3);
+        SetValue(coords.x, coords.y, value);
     }
 }
--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using CodeMonkey.Utils;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GridController : MonoBehaviour
 {
+    private Grid _grid;
     private void Start()
     {
-        Grid grid = new Grid(4, 2, 10f);
+        _grid = new Grid(4, 2, 10f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _grid.SetValue(GetMouseToWorldPosition(), 2);
+        }
+    }
+
+    private Vector3 GetMouseToWorldPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
