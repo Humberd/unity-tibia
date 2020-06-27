aaadb3c03ac140859ddf3c2f08a66159729ab3ba
humberd <humberd2@gmail.com>
2020-06-27 12:53:13 +0200

    Save commit

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
     private CharacterController _controller;
+    public Tilemap tilemap;
+    public Grid grid;
     public float speed = 2.0f;
 
     // Start is called before the first frame update
@@ -16,9 +17,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        var move = getMove();
+        transform.Translate(move);
 
-        _controller.Move(move * (Time.deltaTime * speed));
+
+        var pos = grid.WorldToCell(transform.position);
+        var tileBase = tilemap.GetTile(pos);
+        // Debug.Log(tileBase.name);
+        // Debug.Log(pos);
+
+    }
+
+    private Vector2 getMove()
+    {
+        var isUp = Input.GetKeyDown(KeyCode.W);
+        var isDown = Input.GetKeyDown(KeyCode.S);
+        var isLeft = Input.GetKeyDown(KeyCode.A);
+        var isRight = Input.GetKeyDown(KeyCode.D);
+
+        Vector3 foo = new Vector3();
+        if (isUp)
+        {
+            return Vector2.up;
+        }
+
+        if (isDown)
+        {
+            return Vector2.down;
+        }
+
+        if (isLeft)
+        {
+            return Vector2.left;
+        }
+
+        if (isRight)
+        {
+            return Vector2.right;
+        }
+
+        return Vector2.zero;
     }
 
 }
